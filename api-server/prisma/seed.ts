a89c0b0807ca3575f99cc3cb433d1344e83f377d
Bruno de Castro <bbrezolin@hotmail.com>
2023-04-23 15:48:33 -0300

    mais usuarios exemplo

--- api-server/prisma/seed.ts
+++ api-server/prisma/seed.ts
@@ -5,14 +5,47 @@ const prisma = new PrismaClient();
 async function main() {
     const admin = await prisma.user.create({
         data: {
-            firstName: "Bruno",
-            lastName: "Brezolin",
-            email: "bbrezolin@hotmail.com",
-            password: await hashPassword("batata123"),
+            firstName: "Admin",
+            lastName: "Adminson",
+            email: "admin@admin.com",
+            password: await hashPassword("admin"),
             role: UserRole.Admin,
         }
     });
     console.log(admin);
+
+    const manager = await prisma.user.create({
+        data: {
+            firstName: "Manager",
+            lastName: "Managerson",
+            email: "manager@manager.com",
+            password: await hashPassword("manager"),
+            role: UserRole.Manager,
+        }
+    });
+    console.log(manager);
+
+    const guide = await prisma.user.create({
+        data: {
+            firstName: "Guide",
+            lastName: "Guideson",
+            email: "guide@guide.com",
+            password: await hashPassword("guide"),
+            role: UserRole.Guide,
+        }
+    });
+    console.log(guide);
+
+    const tourist = await prisma.user.create({
+        data: {
+            firstName: "Tourist",
+            lastName: "Touristson",
+            email: "tourist@tourist.com",
+            password: await hashPassword("tourist"),
+            role: UserRole.Tourist,
+        }
+    });
+    console.log(tourist);
 }
 
 main().then(async () => {
